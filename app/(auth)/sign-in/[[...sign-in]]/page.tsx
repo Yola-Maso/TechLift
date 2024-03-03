dc0bd43a753552a5f9225dfaaf0ce264f5e959fd
Yola Maso <myola9567@gmail.com>
2024-03-03 16:31:28 +0200

    initial commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignIn } from '@clerk/nextjs'
+import { Import } from 'lucide-react'
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <SignIn />
+  )
+}
+
+export default SignInPage
